57949a5a9fcc67a936ad413b1c05935a47f51e93
lushpurr <lushpurr@gmail.com>
2021-01-12 17:52:55 +0000

    ITicketed implemented in dodgems

--- src/test/java/attractions/DodgemTest.java
+++ src/test/java/attractions/DodgemTest.java
@@ -2,16 +2,22 @@ package attractions;
 
 import org.junit.Before;
 import org.junit.Test;
+import people.Visitor;
 
 import static org.junit.Assert.assertEquals;
 
 public class DodgemTest {
 
     Dodgems dodgems;
+    Visitor youngVisitor;
+    Visitor oldVisitor;
 
     @Before
     public void setUp() throws Exception {
         dodgems = new Dodgems("Bumper Cars", 5);
+        youngVisitor = new Visitor(10, 1.00, 10.00);
+        oldVisitor = new Visitor(13, 1.50, 10.00);
+
     }
 
 
@@ -28,5 +34,15 @@ public class DodgemTest {
     @Test
     public void hasVisitCount() {
         assertEquals(0, dodgems.getVisitCount());
+    }
+
+    @Test
+    public void areUnder12ChargedHalf(){
+        assertEquals(2.25, dodgems.priceFor(youngVisitor), 0.01);
+    }
+
+    @Test
+    public void areOver12ChargedFullPrice(){
+        assertEquals(4.50, dodgems.priceFor(oldVisitor), 0.01);
     }
 }

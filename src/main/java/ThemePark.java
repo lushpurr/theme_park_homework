891fc9fc0a9dea3113f179d7e3368c43ef81bdf7
lushpurr <lushpurr@gmail.com>
2021-01-13 08:54:43 +0000

    attempted extensions but got stuck

--- src/main/java/ThemePark.java
+++ src/main/java/ThemePark.java
@@ -1,2 +1,21 @@
+import attractions.Attraction;
+import behaviours.IReviewed;
+import stalls.Stall;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class ThemePark {
+
+    private ArrayList<Attraction> attractions;
+    private ArrayList<Stall> stalls;
+
+    public ThemePark(){
+        this.attractions = new ArrayList<Attraction>();
+        this.stalls = new ArrayList<Stall>();
+    }
+
+    public ArrayList<Attraction> getAttractions() {
+        return attractions;
+    }
 }
---
+++ src/test/java/ThemeParkTest.java
@@ -0,0 +1,12 @@
+import behaviours.IReviewed;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class ThemeParkTest {
+
+    @Test
+    public void canGetAttractions(){
+
+    }
+}
